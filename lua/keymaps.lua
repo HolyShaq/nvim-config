3ba2f54e9faa0cf43d72f3281c11417540bb8250
HolyShaq <acebyronehalili@gmail.com>
2024-10-26 15:55:19 +0800

    Modularize init.lua

---
+++ lua/keymaps.lua
@@ -0,0 +1,36 @@
+-- Set <Leader> to space
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Helper function to map keys
+local map = function(mode, lhs, rhs, opts)
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Keybinds to make split navigation easier. Use CTRL+<hjkl> to switch between windows
+map('n', '<M-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<M-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<M-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<M-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- Use CTRL+<,.> to resize windows horizontally
+map('n', '<M-,>', '5<C-w><')
+map('n', '<M-.>', '5<C-w>>')
+-- Use CTRL+<[,> to resize windows vertically
+map('n', '<M-[>', '5<C-w>+')
+map('n', '<M-;>', '5<C-w>-')
+
+-- Map Ctrl+S to save in both normal and insert mode
+map('n', '<C-s>', ':w<CR>')
+map('i', '<C-s>', '<Esc>:w<CR>')
+
+-- NGL idk what this is but its in the kickstart so ima keep it in
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
---
+++ lua/options.lua
@@ -0,0 +1,36 @@
+-- Options
+vim.g.have_nerd_font = true -- Nerd Font
+vim.opt.number = true -- Show line number
+vim.opt.relativenumber = true -- Show relative line number
+vim.opt.mouse = 'a' -- Enable mouse for all modes
+vim.opt.breakindent = true -- Enable break indent
+vim.opt.undofile = true -- Save undo history
+vim.opt.signcolumn = 'yes' -- Sign column
+vim.opt.cursorline = true -- Highlight current line
+vim.opt.showmode = false -- Hide mode
+vim.opt.updatetime = 250 -- Decrease update time
+vim.opt.inccommand = 'split' -- Live preview of substitutions
+
+-- Keep 10 lines above and below the cursor
+vim.opt.scrolloff = 10
+
+-- Sync clipboard between OS and Neovim.
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
