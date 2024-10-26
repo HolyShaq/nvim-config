71f3abdae47fa0737f809a2e5afb7fd4adf37a19
HolyShaq <acebyronehalili@gmail.com>
2024-10-26 16:01:39 +0800

    Plugin structure + added some plugins

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+-- oil
+-- https://github.com/stevearc/oil.nvim
+
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+  config = function()
+    local oil = require 'oil'
+    oil.setup {
+      skip_confirm_for_simple_edits = true,
+      delete_to_trash = true,
+    }
+    vim.keymap.set('n', '-', oil.open_float, { desc = 'Open parent directory' })
+  end,
+}
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+-- toggleterm
+-- https://github.com/akinsho/toggleterm.nvim
+
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  config = function()
+    require('toggleterm').setup {
+      open_mapping = [[<c-\>]],
+      direction = 'float',
+    }
+  end,
+}
