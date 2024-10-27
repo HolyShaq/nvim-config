bce23587bdfbf63eead6337bcd58a56e186010bb
HolyShaq <acebyronehalili@gmail.com>
2024-10-27 12:13:44 +0800

    plugin: smooth cursor for vertical movements

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  {
+    'gen740/SmoothCursor.nvim',
+    config = function()
+      require('smoothcursor').setup {
+        intervals = 10,
+        speed = 15,
+      }
+    end,
+  },
+}
