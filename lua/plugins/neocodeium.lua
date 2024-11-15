3c6d2f3c3d6be139d8c4a766b947259339e55722
HolyShaq <acebyronehalili@gmail.com>
2024-11-15 13:35:12 +0800

    plugin: replace codeium with neocodeium

---
+++ lua/plugins/neocodeium.lua
@@ -0,0 +1,33 @@
+-- neocodeium
+-- https://github.com/monkoose/neocodeium
+
+return {
+  'monkoose/neocodeium',
+  event = 'VeryLazy',
+  config = function()
+    local neocodeium = require 'neocodeium'
+    neocodeium.setup()
+    -- Accept
+    vim.keymap.set('i', '<A-o>', neocodeium.accept)
+
+    -- Accept line
+    vim.keymap.set('i', '<A-i>', neocodeium.accept_line)
+
+    -- Cycle forwards and backwards
+    vim.keymap.set('i', '<A-0>', function()
+      require('neocodeium').sycle_or_complete()
+    end)
+    vim.keymap.set('i', '<A-9>', function()
+      require('neocodeium').cycle_or_complete(-1)
+    end)
+
+    -- Clear suggestions
+    vim.keymap.set('i', '<A-8>', neocodeium.clear)
+
+    -- Open chat
+    vim.keymap.set('n', '<leader>cdc', '<cmd>NeoCodeium chat<cr>', { silent = true })
+
+    -- Toggle Codeium for the buffer
+    vim.keymap.set('n', '<A-O>', '<cmd>NeoCodeium toggle<cr>', { silent = true })
+  end,
+}
