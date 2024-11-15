a0bd79b82f4fff7bb2b41ea9d95471ef7a290d51
HolyShaq <acebyronehalili@gmail.com>
2024-11-15 13:36:00 +0800

    misc: flash comment + outerwilds logo on presence!

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,6 +14,7 @@ return {
     event = 'VeryLazy',
     config = function()
       require('neocord').setup {
+        logo = 'https://cdn2.steamgriddb.com/icon_thumb/0e080857e96278e6dba76ac029faf291.png',
         logo_tooltip = 'The universe is, and we are.',
       }
     end,
