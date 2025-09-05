21d747d4142f02757e331baa7529f59b4c97f109
AceHalili <acebyronehalili.startland@gmail.com>
2025-09-05 17:30:18 +0800

    feat: remove ~ from eob

--- init.lua
+++ init.lua
@@ -777,3 +777,6 @@ require('nvim-treesitter.install').compilers = { 'clang' }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- Remove ugly icky ~ on end of buffer
+vim.opt.fillchars:append({ eob = " " })
